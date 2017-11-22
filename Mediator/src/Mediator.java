c30b9ed1a0cda93bd1c6c72b00df2ec32e8368da
icemonster <nunossabino@hotmail.com>
2017-11-22 15:02:22 +0100

    Started implementing the Server and the Mediator

--- Mediator/src/Mediator.java
+++ Mediator/src/Mediator.java
@@ -1,6 +1,7 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface Mediator_I extends Remote {
+public interface Mediator extends Remote {
     public void addServer (String serverName) throws RemoteException;
+    public void removeServer (String serverName) throws RemoteException;
 }
